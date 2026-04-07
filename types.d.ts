1bb6def9d5e1ee1cb8f3c0725b499a7a767d689d
Aditya Doshi <adityadoshi25@gmail.com>
2026-04-07 20:01:52 +0530

    feat: build add-new-book page

--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,29 @@
+import { UploadSchema } from "./lib/zod";
+
 export interface BookCardProps {
   title: string;
   author: string;
   coverURL: string;
   slug: string;
+}
+
+export type UploadFormInput = z.input<typeof UploadSchema>;
+export type UploadFormValues = z.output<typeof UploadSchema>;
+
+export interface FileUploadFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: FieldPath<T>;
+  label: string;
+  acceptTypes: string[];
+  disabled?: boolean;
+  icon: LucideIcon;
+  placeholder: string;
+  hint: string;
+}
+
+export interface VoiceSelectorProps {
+  disabled?: boolean;
+  className?: string;
+  value?: string;
+  onChange: (voiceId: string) => void;
 }
